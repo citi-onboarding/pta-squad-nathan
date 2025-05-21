632f369cfef9dc9817cc9e3014d78124304803fc
Paulobarrosoo <paulosilvabarrosoo@gmail.com>
2025-05-21 14:41:24 -0300

    feat/datailspage:Terminei a página detalhes da consulta

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,6 +1,10 @@
 import LogoCITi from "./Logo.png";
 import LeftContent from "./LeftContent.png"
 import Buttongroups from "./Buttongroups.png"
+import arrow from "./arrow.png"
+import cat6 from "./cat6.png"
+import ok from "./ok.png"
+import setinha from "./setinha_back.png"
 
-export { LogoCITi, LeftContent, Buttongroups };
+export { LogoCITi, LeftContent, Buttongroups, arrow, cat6, ok, setinha }; 
 
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
     <>
     <div className=" gap-x-3 pb-[20px] pt-[20px] flex h-[114px]  flex-row  relative items-center bg-[#D9D9D9]">
      
-      <Image className="pl-8 w-[200px] h-[74px]" src={LeftContent} alt="Logo" />
+      <Image className="pl-8 ml-[10px] w-[220px] h-[74px]" src={LeftContent} alt="Logo" />
       
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex h-[42px] w-[196px] gap-x-[48px] flex-row justify-start items-center">
 
