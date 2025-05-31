b3ea3218e63ec0564091aad4205db1b201f7500d
brunoramossilva <brunogabriel0900@gmail.com>
2025-05-30 22:52:31 -0300

    refactor(refactor/refactor-register): modified arquive controller structure to organization

--- server/src/controllers/PatientController.ts
+++ server/src/controllers/PatientController.ts
@@ -6,69 +6,9 @@ class RegisterController implements Crud {
   constructor(private readonly citi = new Citi("Animal")) {}
   // Método para criar um novo animal no banco de dados
   create = async (request: Request, response: Response) => {
-    const { patientName,
-        tutorName,
-        patientSpecies,
-        patientAge,
-        typeConsultation,
-        responsibleDoctor,
-        dateService,
-        timeService,
-        problemDescription
-    } = request.body;
 
-    // Verifica se algum dos campos obrigatórios não foi preenchido
-    const isAnyUndefined = this.citi.areValuesUndefined(
-        patientName,
-        tutorName,
-        patientSpecies,
-        patientAge,
-        typeConsultation,
-        responsibleDoctor,
-        dateService,
-        timeService,
-        problemDescription
-    );
-    if (isAnyUndefined) return response.status(400).send();
-
-    // Cria um novo objeto de animal com os dados fornecidos
-    // e insere no banco de dados
-    const newAnimal = { patientName, tutorName, patientSpecies, patientAge, typeConsultation, responsibleDoctor, dateService, timeService, problemDescription };
-    const { httpStatus, message } = await this.citi.insertIntoDatabase(newAnimal);
-    // Retorna a resposta com o status HTTP e a mensagem de sucesso ou erro
-    return response.status(httpStatus).send({ message });
-  };
-  // Método para obter todos os animais do banco de dados
-  get = async (request: Request, response: Response) => {
-    const {httpStatus, values} = await this.citi.getAll();
-
-    return response.status(httpStatus).send(values);
-  };
-  // Método para deletar um animal do banco de dados
-  delete = async (request: Request, response: Response) => {
-    const { id } = request.params;
-
-    const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
-
-    return response.status(httpStatus).send({ messageFromDelete });
-  };
-
-  // Método para atualizar os dados de um animal no banco de dados
-  update = async (request: Request, response: Response) => {
-    const { id } = request.params
-
-    const { patientName,
-        tutorName,
-        patientSpecies,
-        patientAge,
-        typeConsultation,
-        responsibleDoctor,
-        dateService,
-        timeService,
-        problemDescription
-    } = request.body;
-
-    const updatedValues = { patientName,
+    const {
+      patientName,
       tutorName,
       patientSpecies,
       patientAge,
@@ -76,7 +16,85 @@ class RegisterController implements Crud {
       responsibleDoctor,
       dateService,
       timeService,
-      problemDescription };
+      problemDescription
+    } = request.body;
+
+    // Verifica se algum dos campos obrigatórios não foi preenchido
+    const isAnyUndefined = this.citi.areValuesUndefined(
+      patientName,
+      tutorName,
+      patientSpecies,
+      patientAge,
+      typeConsultation,
+      responsibleDoctor,
+      dateService,
+      timeService,
+      problemDescription
+    );
+    if (isAnyUndefined) return response.status(400).send();
+
+    // Cria um novo objeto de animal com os dados fornecidos e insere no banco de dados
+    const newAnimal = {
+      patientName,
+      tutorName,
+      patientSpecies,
+      patientAge,
+      typeConsultation,
+      responsibleDoctor,
+      dateService,
+      timeService,
+      problemDescription
+    };
+    const { httpStatus, message } = await this.citi.insertIntoDatabase(newAnimal);
+    // Retorna a resposta com o status HTTP e a mensagem de sucesso ou erro
+    return response.status(httpStatus).send({ message });
+
+  };
+
+  // Método para obter todos os animais do banco de dados
+  get = async (request: Request, response: Response) => {
+    const {httpStatus, values} = await this.citi.getAll();
+    
+    return response.status(httpStatus).send(values);
+  };
+
+  // Método para deletar um animal do banco de dados
+  delete = async (request: Request, response: Response) => {
+    const { id } = request.params;
+
+    const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
+
+    return response.status(httpStatus).send({ messageFromDelete });
+
+  };
+
+  // Método para atualizar os dados de um animal no banco de dados
+  update = async (request: Request, response: Response) => {
+    const { id } = request.params
+
+    const {
+      patientName,
+      tutorName,
+      patientSpecies,
+      patientAge,
+      typeConsultation,
+      responsibleDoctor,
+      dateService,
+      timeService,
+      problemDescription
+    } = request.body;
+
+    const updatedValues = {
+      patientName,
+      tutorName,
+      patientSpecies,
+      patientAge,
+      typeConsultation,
+      responsibleDoctor,
+      dateService,
+      timeService,
+      problemDescription
+    };
 
     const  { httpStatus, messageFromUpdate } = await this.citi.updateValue(
       id,
