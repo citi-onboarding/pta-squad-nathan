940551a1e8894de7156940b3ea4333a93c7bee30
Nathan Barbosa <nathansantos1713@gmail.com>
2025-06-01 19:23:04 -0300

    feat(Header): adding navigation link

--- client/src/app/detalhes/page.tsx
+++ client/src/app/detalhes/page.tsx
@@ -1,126 +1,160 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
-import Header from "@/components/Header"
-import Image from "next/image"
-import { arrow } from "@/assets"
-import { cat6 } from "@/assets"
-import { ok } from "@/assets"
-import ConsultCard from "@/components/ConsultCard"
-import ConsultaModal from "@/components/Consultamodal"
-import { useState } from "react"
+import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
+import Image from "next/image";
+import { arrow } from "@/assets";
+import { cat6 } from "@/assets";
+import { ok } from "@/assets";
+import ConsultCard from "@/components/ConsultCard";
+import ConsultaModal from "@/components/Consultamodal";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Details() {
-  const[openmodal, setOpenModal] = useState(false)
-  
+  const [openmodal, setOpenModal] = useState(false);
+
   return (
-    <>
+    <div className="h-max">
       <Header />
-      <ConsultaModal isOpen={openmodal} setModalOpen={() => setOpenModal (!openmodal)}/>
+      <ConsultaModal
+        isOpen={openmodal}
+        setModalOpen={() => setOpenModal(!openmodal)}
+      />
       <div className="flex flex-row h-full w-full justify-around  bg-[#FFFFFF] text-black mt-14">
+        {/* COMEÇO LADO ESQUERDO */}
 
-       
+        <div className="flex-1 flex flex-col px-4 md:px-0 md:ml-20 lg:ml-32">
+          {/* detalhes da consulta */}
 
-      {/* COMEÇO LADO ESQUERDO */}
-    
-      <div className="flex-1 flex flex-col px-4 md:px-0 md:ml-20 lg:ml-32"> 
-
-        {/* detalhes da consulta */}
-        
-        <div className="flex flex-row items-baseline mb-8"> 
-          <Image className="w-8 h-8 mr-4" src={arrow} alt="arrow" />
-          <h1 className="text-5xl font-bold leading-none -mt-4">Detalhes da consulta</h1> 
-        </div>
-
-        {/* paciente */}
-        
-        <div className="flex mt-3">
-          <h2 className="font-bold text-2xl">Paciente</h2>
-        </div>
-
-        {/* foto do gato + componentes */}
-       
-        <div className="flex flex-col sm:flex-row mt-8 items-start"> 
-          <Image className="w-[18.4375rem] h-[18.6875rem] max-w-full" src={cat6} alt="gato" />
-
-          
-          <div className="flex flex-col ml-6 sm:ml-6 mt-4 sm:mt-0 space-y-2"> 
-            <h2 className="font-bold text-lg">Luna</h2> 
-            <h2 className="text-base">5 anos</h2> 
+          <div className="flex flex-row items-baseline mb-8">
+            <Link href={"/"}>
+              <Image className="w-8 h-8 mr-4" src={arrow} alt="arrow" />
+            </Link>
+            <h1 className="text-5xl font-bold leading-none -mt-4">
+              Detalhes da consulta
+            </h1>
           </div>
 
-        
-          <div className="flex flex-col -ml-11  mt-[15rem] space-y-2"> 
-            <h2 className="text-base">Lucas Gomes</h2> 
-            <h2 className="text-base">Dr. José Carlos</h2>
-          </div>
-        </div>
+          {/* paciente */}
 
-        {/* descrição */}
-     
-        <div className="flex mt-16 flex-col w-full md:max-w-xl h-auto"> 
-          <h2 className="font-bold text-base">Descrição do problema:</h2>
-          <p className="mt-2 text-sm "> 
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque dolore sunt accusamus, similique beatae inventore distinctio fuga ipsum cum eos, laboriosam consequatur recusandae consequuntur obcaecati asperiores illum dicta quibusdam velit!
-          </p>
-        </div>
-
-        {/* Tipo de consulta */}
-        
-        <div className="flex flex-row w-full sm:w-fit h-fit mt-12 items-center"> 
-          <h2 className="font-bold text-base">Tipo de consulta:</h2>
-          
-          <div className="w-[6.3125rem] h-[1.875rem] bg-[#AAE1FF] ml-8 rounded flex items-center justify-center"> 
-            <p className="text-sm">Vacinação</p> 
+          <div className="flex mt-3">
+            <h2 className="font-bold text-2xl">Paciente</h2>
           </div>
 
-        </div>
+          {/* foto do gato + componentes */}
 
-      
-        <div className="w-full md:max-w-2xl border border-[#D9D9D9] rounded-3xl mt-10 p-6"> 
-         
-          <div className="flex flex-col items-center justify-center gap-y-5">
-            <h2 className="font-bold text-base">Deseja realizar outra consulta?</h2> 
-            <div className="flex justify-center w-full"> 
+          <div className="flex flex-col sm:flex-row mt-8 items-start">
+            <Image
+              className="w-[18.4375rem] h-[18.6875rem] max-w-full"
+              src={cat6}
+              alt="gato"
+            />
 
-              
-              <Button
-                onClick={() => setOpenModal(true)}
-                className="w-[38rem] h-12 bg-[#50E678] border rounded-full flex items-center justify-center gap-x-2 text-base font-bold" > 
-                <Image className="w-6 h-6" src={ok} alt="ok" />
-                Agendamento
-              </Button>
+            <div className="flex flex-col ml-6 sm:ml-6 mt-4 sm:mt-0 space-y-2">
+              <h2 className="font-bold text-lg">Luna</h2>
+              <h2 className="text-base">5 anos</h2>
+            </div>
+
+            <div className="flex flex-col -ml-11  mt-[15rem] space-y-2">
+              <h2 className="text-base">Lucas Gomes</h2>
+              <h2 className="text-base">Dr. José Carlos</h2>
+            </div>
+          </div>
+
+          {/* descrição */}
+
+          <div className="flex mt-16 flex-col w-full md:max-w-xl h-auto">
+            <h2 className="font-bold text-base">Descrição do problema:</h2>
+            <p className="mt-2 text-sm ">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+              dolore sunt accusamus, similique beatae inventore distinctio fuga
+              ipsum cum eos, laboriosam consequatur recusandae consequuntur
+              obcaecati asperiores illum dicta quibusdam velit!
+            </p>
+          </div>
+
+          {/* Tipo de consulta */}
+
+          <div className="flex flex-row w-full sm:w-fit h-fit mt-12 items-center">
+            <h2 className="font-bold text-base">Tipo de consulta:</h2>
+
+            <div className="w-[6.3125rem] h-[1.875rem] bg-[#AAE1FF] ml-8 rounded flex items-center justify-center">
+              <p className="text-sm">Vacinação</p>
+            </div>
+          </div>
+
+          <div className="w-full md:max-w-2xl border border-[#D9D9D9] rounded-3xl mt-10 p-6">
+            <div className="flex flex-col items-center justify-center gap-y-5">
+              <h2 className="font-bold text-base">
+                Deseja realizar outra consulta?
+              </h2>
+              <div className="flex justify-center w-full">
+                <Button
+                  onClick={() => setOpenModal(true)}
+                  className="w-[38rem] h-12 bg-[#50E678] border rounded-full flex items-center justify-center gap-x-2 text-base font-bold"
+                >
+                  <Image className="w-6 h-6" src={ok} alt="ok" />
+                  Agendamento
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* Fim lado esquerdo */}
+        {/* Fim lado esquerdo */}
 
-       {/* COMEÇO LADO DIREITO */}
-      
-      <div className="flex h-full flex-col mr-[8%] mt-[5%] w-full px-4 md:px-0 md:max-w-md lg:max-w-lg"> 
+        {/* COMEÇO LADO DIREITO */}
 
-        {/* Histórico de consultas */}
-        <h2 className="font-bold text-2xl mb-4">Histórico de consultas</h2>
+        <div className="flex h-full flex-col mr-[8%] mt-[5%] w-full px-4 md:px-0 md:max-w-md lg:max-w-lg">
+          {/* Histórico de consultas */}
+          <h2 className="font-bold text-2xl mb-4">Histórico de consultas</h2>
 
-        {/* div de consultas */}
-      
-        <div className="w-full h-[45%] border border-[#D9D9D9] rounded-3xl mt-6 p-6 flex flex-col items-center gap-y-4"> 
+          {/* div de consultas */}
 
-          <ConsultCard date="18/02" time="13:00" title="Primeira consulta" doctor="Dr. José Carlos" />
-          <ConsultCard date="20/03" time="13:00" title="Primeira consulta" doctor="Dr. José Carlos" />
-          <ConsultCard date="15/04" time="14:00" title="Primeira consulta" doctor="Dr. José Carlos" />
-          <ConsultCard date="20/03" time="13:00" title="Primeira consulta" doctor="Dr. José Carlos" />
-            
-
+          <div className="w-full h-[45%] border border-[#D9D9D9] rounded-3xl mt-6 p-6 flex flex-col items-center gap-y-4">
+            <Link href={"/detalhes"}>
+              <ConsultCard
+                date="18/02"
+                time="13:00"
+                title="Primeira consulta"
+                doctor="Dr. José Carlos"
+              />
+            </Link>
+            <Link href={"/detalhes"}>
+              <ConsultCard
+                date="18/02"
+                time="13:00"
+                title="Primeira consulta"
+                doctor="Dr. José Carlos"
+              />
+            </Link>
+            <Link href={"/detalhes"}>
+              <ConsultCard
+                date="18/02"
+                time="13:00"
+                title="Primeira consulta"
+                doctor="Dr. José Carlos"
+              />
+            </Link>
+            <Link href={"/detalhes"}>
+              <ConsultCard
+                date="18/02"
+                time="13:00"
+                title="Primeira consulta"
+                doctor="Dr. José Carlos"
+              />
+            </Link>
+            <Link href={"/detalhes"}>
+              <ConsultCard
+                date="18/02"
+                time="13:00"
+                title="Primeira consulta"
+                doctor="Dr. José Carlos"
+              />
+            </Link>
           </div>
-
         </div>
-
       </div>
-
-    </>
-
-
-  )
+    </div>
+  );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,48 +1,55 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { useState } from "react";
-import {LeftContent} from "@/assets";
-import {Buttongroups} from "@/assets"
-
-
-
-
+import { LeftContent } from "@/assets";
+import { Buttongroups } from "@/assets";
+import Link from "next/link";
 
 export default function Header() {
   const [active, setActive] = useState("atendimento");
   return (
     <>
-    <div className=" gap-x-3 pb-5 pt-5 flex h-17  flex-row  relative items-center bg-[#FFFFFF]">
-     
-      <Image className="pl-8 ml-2.5 w-56 h-18" src={LeftContent} alt="Logo" />
-      
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex h-11 w-48 gap-x-12 flex-row justify-start items-center">
+      <div className=" gap-x-3 pb-5 pt-5 flex h-17  flex-row  relative items-center bg-[#FFFFFF]">
+        <Link href={"/"}>
+          <Image
+            className="pl-8 ml-2.5 w-56 h-18"
+            src={LeftContent}
+            alt="Logo"
+          />
+        </Link>
 
-        <div className= " flex flex-col items-center cursor pointer" onClick={() => setActive("atendimento")}>
-        <h2>Atendimento</h2>
-        {active === "atendimento" && (
-            <div className="w-full h-1 bg-green-500 rounded" />
-          )}
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex h-11 w-48 gap-x-12 flex-row justify-start items-center">
+          <Link href={"/"}>
+            <div
+              className=" flex flex-col items-center cursor pointer"
+              onClick={() => setActive("atendimento")}
+            >
+              <h2>Atendimento</h2>
+              {active === "atendimento" && (
+                <div className="w-full h-1 bg-green-500 rounded" />
+              )}
+            </div>
+          </Link>
+          
+          <Link href={"/registro"}>
+            <div
+              className="flex flex-col items-center cursor pointer"
+              onClick={() => setActive("cadastro")}
+            >
+              <h2>Cadastro</h2>
+              {active === "cadastro" && (
+                <div className="w-full h-1 bg-green-500 rounded" />
+              )}
+            </div>
+          </Link>
         </div>
 
-        <div className= "flex flex-col items-center cursor pointer" onClick={() => setActive("cadastro")}>
-        <h2>Cadastro</h2>
-        {active === "cadastro" && (
-            <div className="w-full h-1 bg-green-500 rounded" />
-          )}
+        <div className="ml-auto pr-8">
+          <Image className="w-56 h-6 gap-x-2" src={Buttongroups} alt="Logo" />
         </div>
-        
-      </div>
-
-      <div className="ml-auto pr-8">
-        <Image className="w-56 h-6 gap-x-2" src={Buttongroups} alt="Logo" />
-      </div>
-
       </div>
 
       <div className="w-full h-0.5 bg-[#D9D9D9]"></div>
-      </>
-      
-    
+    </>
   );
 }
