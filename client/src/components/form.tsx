62e618140de030784101196c3ea278db8869188c
brunoramossilva <brunogabriel0900@gmail.com>
2025-06-01 19:14:11 -0300

    feat(feat/axios-integration-register): start validations

--- client/src/components/form.tsx
+++ client/src/components/form.tsx
@@ -7,6 +7,7 @@ import { AnimalSelector } from "./animalSelector"
 import { RegisterModal } from "./registerModal"
 import { arrowBack, arrowDown } from "@/assets"
 import api from "@/services/api"
+import { useForm } from "react-hook-form"
 import { Button } from "./ui/button"
 
 // Nossa função principal, que importa os componentes necessários
@@ -39,16 +40,8 @@ function Form({ setModalAberto }: { setModalAberto: (open: boolean) => void }) {
         setEspecieSelecionada(especie);
       }
   };
-
-  const [nomePaciente, setNomePaciente] = useState("");
-  const [nomeTutor, setNomeTutor] = useState("");
-  const [genero, setGenero] = useState("");
-  const [dataNascimento, setDataNascimento] = useState("");
-  const [tipoConsulta, setTipoConsulta] = useState("");
-  const [medicoResponsavel, setMedicoResponsavel] = useState("");
-  const [dataAtendimento, setDataAtendimento] = useState("");
-  const [horaAtendimento, setHoraAtendimento] = useState("");
-  const [descricao, setDescricao] = useState("");
+  
+  const { register, handleSubmit, formState: { errors },reset } = useForm();
 
   // Estilos padrões para os campos - gerando economia de tempo e reutilização
   const labelStyles = "block mb-1 font-sf font-bold";
@@ -57,51 +50,53 @@ function Form({ setModalAberto }: { setModalAberto: (open: boolean) => void }) {
   
   // Função para lidar com o envio do formulário
   // A função é assíncrona e usa o método POST da API para enviar os dados do formulário
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    
+  const onSubmit = async (dataForm: any) => {
     if (!especieSelecionada) {
       alert("Selecione uma espécie.");
       return;
-    }
-    
-    const datetime = new Date(`${dataAtendimento}T${horaAtendimento}:00Z`).toISOString();
-    
-    const data = {
-      name: nomePaciente,
-      tutorName: nomeTutor,
-      age: dataNascimento,
-      species: especieSelecionada.toUpperCase(),
-      gender: genero.toUpperCase(),
-      doctorName: medicoResponsavel,
-      datetime: datetime,
-      type: tipoConsulta.toUpperCase(),
-      description: descricao,
-    };
-    
-    try {
-      const response = await api.post("/consultations", data);
-      console.log("Enviado com sucesso:", response.data);
-      setModalAberto(true);
-    } catch (error) {
-      console.error("Erro ao enviar:", error);
-      alert("Erro ao cadastrar.");
-    }
+  }
+  
+  const datetime = new Date(`${dataForm.dataAtendimento}T${dataForm.horaAtendimento}:00Z`).toISOString();
+  
+  const data = {
+    name: dataForm.nomePaciente,
+    tutorName: dataForm.nomeTutor,
+    age: dataForm.dataNascimento,
+    species: especieSelecionada,
+    gender: dataForm.genero,
+    doctorName: dataForm.medicoResponsavel,
+    datetime,
+    type: dataForm.tipoConsulta,
+    description: dataForm.descricao,
   };
+
+  try {
+    const response = await api.post("/consultations", data);
+    console.log("Enviado com sucesso:", response.data);
+    setModalAberto(true);
+    reset();
+  } catch (error) {
+    console.error("Erro ao enviar:", error);
+    alert("Erro ao cadastrar.");
+  }
+};
  
   return (
-    // Define as dimensões do formulário e o espaçamento entre os elementos
+    // Define as dimensões do formulário
     <form
     id="form-cadastro"
-    className="max-w-screen-2xl mx-auto space-y-3 mt-6 p-4 sm:p-0"
-    onSubmit={handleSubmit}
+    className="max-w-screen-2xl mx-auto space-y-3 mt-4 p-4 sm:p-0"
+    onSubmit={handleSubmit(onSubmit)}
     >
 
       {/* Botão de voltar para a página de cadastro*/}
       <button>
-        <div className="flex items-center mb-5">
+        <div className="flex items-center mb-4">
           <Image className="sm:w-4 sm:h-7" src={arrowBack} alt="arrowBack"/>
-          <label className="font-source text-4xl sm:text-5xl font-bold ml-6 cursor-pointer">Cadastro</label>
+          <label
+          className="font-source text-4xl sm:text-5xl font-bold ml-6 cursor-pointer">
+            Cadastro
+            </label>
         </div>
       </button>
 
@@ -111,57 +106,72 @@ function Form({ setModalAberto }: { setModalAberto: (open: boolean) => void }) {
       {/* O campo abaixo é dividido em duas partes: nome do paciente e nome do tutor */}
       <div className={blocksStyles}>
         <div className="flex-1">
-          <label className={labelStyles}>Nome do Paciente</label>
+          <label className={labelStyles}>Nome do paciente</label>
           <input
-          type="text" placeholder="Digite aqui..." className={placeHolderStyles}
-          value={nomePaciente}
-          onChange={(e) => setNomePaciente(e.target.value)}
+          className={`${placeHolderStyles} ${errors?.nomePaciente ? "input-error border-red-500" : ""}`}
+          type="text" placeholder="Digite aqui..."
+          {...register("nomePaciente", { required: true })}
           />
+          {errors?.nomePaciente?.type === "required" && <p className="error-message text-red-500 text-xs mt-0.5">É obrigatório informar o nome do paciente.</p>}
         </div>
 
         {/* Insere o campo do nome do tutor */}
         <div className="flex-1 mb-2">
-          <label className={labelStyles}>Nome do Tutor</label>
+          <label className={labelStyles}>Nome do tutor</label>
           <input
-          type="text" placeholder="Digite aqui..." className={placeHolderStyles}
-          value={nomeTutor}
-          onChange={(e) => setNomeTutor(e.target.value)}
+          className={`${placeHolderStyles} ${errors?.nomeTutor ? "input-error border-red-500" : ""}`}
+          type="text" placeholder="Digite aqui..."
+          {...register("nomeTutor", { required: true })}
           />
+          {errors?.nomeTutor?.type === "required" && <p className="error-message text-red-500 text-xs mt-0.5">É obrigatório informar o nome do tutor do paciente.</p>}
         </div>
       </div>
       
 
       {/* Insere o campo da espécie do paciente */}
       {/* O componente AnimalSelector é responsável por renderizar as opções de espécies */}
-      <label className={labelStyles}>Qual é a Espécie do Paciente?</label>
+      <label className={labelStyles}>Qual é a espécie do paciente?</label>
       <AnimalSelector selected={especieSelecionada} onSelect={handleSelecionarEspecie}/>
 
 
       {/* O campo abaixo é dividido em três partes: gênero do paciente, idade e tipo de consulta */}
       <div className={blocksStyles}>
         <div className="flex-1">
-          <label className={labelStyles}>Gênero do Paciente</label>
-          <div className="border rounded-lg border-black relative">
+          <label className={labelStyles}>Gênero do paciente</label>
+          <div className={`border rounded-lg relative ${errors?.genero ? "border-red-500" : "border-black"}`}>
 
             <select className="w-full p-2 appearance-none bg-transparent"
-              value={genero}
-              onChange={(e) => setGenero(e.target.value)}
+            {...register("genero", {
+              validate: (value) => value !== "" || "É obrigatório informar o gênero do paciente.",
+            })}
+            defaultValue=""
               >
                 <option value="" disabled>Selecione aqui</option>
                 <option value="FEMALE">Fêmea</option>
                 <option value="MALE">Macho</option>
             </select>
 
-            <Image src={arrowDown} className="absolute right-5 top-1/2 -translate-y-1/2 w-3 h-2" alt="arrowDown"/>
+            <Image
+            src={arrowDown}
+            className="absolute right-5 top-1/2 -translate-y-1/2 w-3 h-2"
+            alt="arrowDown"
+            />
+
           </div>
+
+          {errors.genero && (
+            <p className="text-red-500 text-xs mt-0.5">
+              {errors.genero.message?.toString()}
+              </p>
+            )}
+
         </div>
 
         <div className="flex-1">
-          <label className={labelStyles}>Data de Nascimento do Paciente</label>
+          <label className={labelStyles}>Data de nascimento do paciente</label>
           <input
           type="date" className={placeHolderStyles}
-          value={dataNascimento}
-          onChange={(e) => setDataNascimento(e.target.value)}
+          {...register("dataNascimento")}
           />
         </div>
 
@@ -169,18 +179,18 @@ function Form({ setModalAberto }: { setModalAberto: (open: boolean) => void }) {
         {/* O campo é um select com opções pré-definidas */}
         {/* A imagem arrowDown é usada como um ícone para indicar que é um campo de seleção */}
         <div className="flex-1">
-          <label className={labelStyles}>Tipo de Consulta</label>
+          <label className={labelStyles}>Tipo de consulta</label>
           <div className="border rounded-lg border-black relative">
 
             <select className="w-full p-2 appearance-none bg-transparent"
-            value={tipoConsulta}
-            onChange={(e) => setTipoConsulta(e.target.value)}
+            {...register("tipoConsulta")}
+            defaultValue=""
             >
               <option value="" disabled>Selecione aqui</option>
-              <option value="FIRST">Primeira Consulta</option>
+              <option value="FIRST">Primeira consulta</option>
               <option value="VACINATION">Vacinação</option>
-              <option value="RETURN">Retorno de Consulta</option>
-              <option value="CHECKUP">Check-up Geral</option>
+              <option value="RETURN">Retorno de consulta</option>
+              <option value="CHECKUP">Check-up geral</option>
             </select>
 
             <Image src={arrowDown} className="absolute right-5 top-1/2 -translate-y-1/2 w-3 h-2" alt="arrowDown"/>
@@ -192,29 +202,27 @@ function Form({ setModalAberto }: { setModalAberto: (open: boolean) => void }) {
       {/* O campo abaixo é dividido em três partes: médico responsável, data do atendimento e horário do atendimento */}
       <div className={blocksStyles}>
         <div className="flex-1">
-          <label className={labelStyles}>Médico Responsável</label>
+          <label className={labelStyles}>Médico responsável</label>
           <input
           type="text" placeholder="Digite aqui..." className={placeHolderStyles}
-          value={medicoResponsavel}
-          onChange={(e) => setMedicoResponsavel(e.target.value)}
+          {...register("medicoResponsavel")}
           />
         </div>
 
         <div className="flex-1">
-          <label className={labelStyles}>Data do Atendimento</label>
+          <label className={labelStyles}>Data do atendimento</label>
           <input
           type="date" className={placeHolderStyles}
-          value={dataAtendimento}
-          onChange={(e) => setDataAtendimento(e.target.value)}
+          {...register("dataAtendimento")}
           />
         </div>
 
         <div className="flex-1">
-          <label className={labelStyles}>Horário do Atendimento</label>
+          <label className={labelStyles}>Horário do atendimento</label>
           <input
           type="time" className={placeHolderStyles}
-          value={horaAtendimento}
-          onChange={(e) => setHoraAtendimento(e.target.value)}
+          {...register("horaAtendimento")}
+          defaultValue="00:00"
           />
         </div>
       </div>
@@ -222,17 +230,16 @@ function Form({ setModalAberto }: { setModalAberto: (open: boolean) => void }) {
 
       {/* Insere o campo da descrição do problema */}
       {/* O campo é um textarea com um tamanho mínimo definido */}
-      <label className={labelStyles}>Descrição do Problema</label>
+      <label className={labelStyles}>Descrição do problema</label>
       <textarea
       placeholder="Digite aqui..." className="w-full p-3 border rounded-lg border-black min-h-24"
-      value={descricao}
-      onChange={(e) => setDescricao(e.target.value)}
+      {...register("descricao")}
       />
       
       <div className="flex justify-center sm:justify-end max-w-screen-2xl mx-auto">
         <Button
         type="submit"
-        className="w-auto h-auto font-sf font-bold sm:w-52 sm:h-12 bg-[#50E678] hover:bg-[#40C768] text-white rounded-full transition sm:mt-12 mb-4 sm:mb-0"
+        className="w-auto h-auto font-sf font-bold sm:w-52 sm:h-12 bg-[#50E678] hover:bg-[#40C768] text-white rounded-full transition sm:mt-4 mb-4 sm:mb-0"
         >
           Finalizar Cadastro
         </Button>
