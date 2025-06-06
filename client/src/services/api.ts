2bc8cfdcd3c2dbf589fafaf049c6f262994cf815
Nathan Barbosa <nathansantos1713@gmail.com>
2025-06-06 10:56:55 -0300

    fix(api): altera a URL base da API para localhost

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pta-squad-nathan-server.onrender.com/api",
+  baseURL: "http://localhost:3001/api",
   headers: {
     "Content-Type": "application/json",
   },
